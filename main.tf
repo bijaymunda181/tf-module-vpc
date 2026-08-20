fcba79039a9c8f163f113447c9efface366c59b6
Bijay <bijay78082@gmail.com>
2026-08-20 14:29:02 +0530

    terraform

--- main.tf
+++ main.tf
@@ -63,7 +63,41 @@ resource "aws_route" "default-vpc-peer-entry" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
+resource "aws_instance" "main" {
+  instance_type = "t3.micro"
+  ami = "ami-00266e6d3c0dc79bf"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  subnet_id = local.app_subnet_ids[0]
 
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+}
 output "subnet" {
   value = module.subnets
 }
