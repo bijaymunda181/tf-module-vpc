fb420893fc4e5672c6a9e6b15f133543b1666ead
Bijay <bijay78082@gmail.com>
2026-08-20 13:17:17 +0530

    terraform

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "igw" {
-  for_each                     = lookup(lookup(module.subnets, "public", null ), "route_table_ids", null)
-  route_table_id            = each.value["id"]
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id            = aws_internet_gateway.igw.id
+  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id         = each.value["id"]
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route" "ngw" {
